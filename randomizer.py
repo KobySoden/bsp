8c83459615c83f61983feb34c571686687f0b724
Koby L Soden <ksoden@bec307dl08.sandiego.edu>
2022-05-02 16:37:59 -0700

    added something to the randomizer that prevents bad random number generations from being returned

--- randomizer.py
+++ randomizer.py
@@ -20,6 +20,14 @@ def middle_squares(seed, x):
         seed = int(str(squared)[a:b])
         #print(seed % 38)
         vals[seed % 38] += 1
+        #I think we should add a conditional here to scrap bad runs
+        if vals[seed % 38] > x/3:
+            print("bad run")
+            vals = []
+            for i in range(38):
+                vals.append(0)
+            middle_squares(int(sys.argv[1])+1, 100)
+            return;
 
 middle_squares(sys.argv[1], 100)
 
