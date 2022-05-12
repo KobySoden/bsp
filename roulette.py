5f481767e8f95aea3c15d7d8fc83c40e210ca544
Brady R Fischer <bfischer@bec307dl10.sandiego.edu>
2022-05-11 18:36:33 -0700

    fixed some formatting and added odds to init

--- roulette.py
+++ roulette.py
@@ -26,6 +26,10 @@ class Roulette():
         self.reds = [1,3,5,7,9,12,14,16,18,19,21,23,25,27,30,32,34,36]
         self.blacks = [2,4,6,8,10,11,13,15,17,20,22,24,26,28,29,31,33,35]
         self.greens = [0,37]
+        print("In case you don't know, here are your odds!")
+        print("Betting on red/black or even/odd pays 1 to 1.")
+        print("Betting on green pays 17 to 1.")
+        print("Betting on an individual number pays 35 to 1.")
         if self.firstrun:
             self.welcome()
         else:
@@ -34,10 +38,10 @@ class Roulette():
     def welcome(self):
         print("Welcome to Team D's Roulette Game!")
         print("You'll start with $100 to play with.")
-        inp = input("Type 1 to see the betting odds, or type 2 to begin the game.")
+        inp = input("Type 1 to see the betting odds, or type 2 to begin the game. ")
         while inp not in ["1", "2"]:
             print("Please input a 1 or a 2.")
-            inp = input("Type 1 to see the betting odds, or type 2 to begin the game.")
+            inp = input("Type 1 to see the betting odds, or type 2 to begin the game. ")
         if inp == "1":
             print("Betting on red/black or even/odd pays 1 to 1.")
             print("Betting on green pays 17 to 1.")
@@ -45,24 +49,24 @@ class Roulette():
         else:
             self.spin()
             return
-        inp = input("Begin game?: y/n")
+        inp = input("Begin game?: y/n ")
         while inp not in ["y", "n"]:
-            inp = input("Begin game?: y/n")
+            inp = input("Begin game?: y/n ")
         if inp == "y":
             self.spin()
         else:
             self.exit(False)
     def spin(self):
         print("You have $" + str(self.money) + " remaining.")
-        inp = input("Type 1 to bet on a color, 2 to bet on even/odd, or 3 to bet on an individual number.")
+        inp = input("Type 1 to bet on a color, 2 to bet on even/odd, or 3 to bet on an individual number. ")
         while inp not in ["1", "2", "3"]:
             print("Please input 1, 2, or 3.")
-            inp = input("Type 1 to bet on a color, 2 to bet on even/odd, or 3 to bet on an individual number.")
+            inp = input("Type 1 to bet on a color, 2 to bet on even/odd, or 3 to bet on an individual number. ")
         if inp == "1":
-            inp = input("Type 1 to bet on red, 2 to bet on black, or 3 to bet on green.")
+            inp = input("Type 1 to bet on red, 2 to bet on black, or 3 to bet on green. ")
             while inp not in ["1", "2", "3"]:
                 print("Please input 1, 2, or 3.")
-                inp = input("Type 1 to bet on red, 2 to bet on black, or 3 to bet on green.")
+                inp = input("Type 1 to bet on red, 2 to bet on black, or 3 to bet on green. ")
             bet = self.getbet()
             print("Spinning...")
             spin_result = self.numbers.pop()
@@ -77,10 +81,10 @@ class Roulette():
                 print("You lose.")
                 self.money -= int(bet)
         elif inp == "2":
-            inp = input("Type 1 to bet on even or 2 to bet on odd.")
+            inp = input("Type 1 to bet on even or 2 to bet on odd. ")
             while inp not in ["1","2"]:
                 print("Please input 1 or 2.")
-                inp = input("Type 1 to bet on even or 2 to bet on odd.")
+                inp = input("Type 1 to bet on even or 2 to bet on odd. ")
             bet = self.getbet()
             print("Spinning...")
             spin_result = self.numbers.pop()
@@ -99,14 +103,14 @@ class Roulette():
                 print("You lose.")
                 self.money -= int(bet)
         elif inp == "3":
-            inp = input("Please type the number you would like to bet on.")
+            inp = input("Please type the number you would like to bet on. ")
             legal_inputs = list(range(0, 38))
             for i in range(len(legal_inputs)):
                 legal_inputs[i] = str(legal_inputs[i])
             legal_inputs.append("00")
             while inp not in legal_inputs:
                 print("Please input a number from 0 to 36 including 00.")
-                inp = input("Please type the number you would like to bet on.")
+                inp = input("Please type the number you would like to bet on. ")
             if inp == "00":
                 inp = "37"
             bet = self.getbet()
@@ -134,15 +138,15 @@ class Roulette():
             else:
                 self.spin()
     def getbet(self):
-        bet = input("How much would you like to bet?")
+        bet = input("How much would you like to bet? ")
         valid_bet = False
         while not valid_bet:
             if not bet.isnumeric():
                 print("Please input a number.")
-                bet = input("How much would you like to bet?")
+                bet = input("How much would you like to bet? ")
             elif int(bet) > self.money:
                 print("Please input a number less than or equal to your current balance ($", self.money, ")")
-                bet = input("How much would you like to bet?")
+                bet = input("How much would you like to bet? ")
             else:
                 valid_bet = True
         return int(bet)
