911c4923f7b93ddc7df96742c1f49c8ca684eda9
Koby L Soden <ksoden@bec307dl08.sandiego.edu>
2022-05-11 19:10:22 -0700

    added test script

---
+++ randomizer_test.py
@@ -0,0 +1,46 @@
+import sys
+import math
+
+test = True
+
+#histogram 
+vals = []
+for i in range(38):
+    vals.append(0)
+
+def middle_squares(seed, x):
+    seed = abs(int(seed))
+    #print(seed)
+    n = len(str(seed))
+    if n % 2 != 0:
+        seed = int(str(seed)[1:])
+    n = len(str(seed))
+    
+    #variables for storing values
+    global vals
+    queue = []
+    
+    for i in range(x):
+        squared = str(seed ** 2).zfill(2 * n)
+        a = n // 2
+        b = a + n
+        seed = int(str(squared)[a:b])
+        
+        vals[seed % 38] += 1
+        queue.append(seed % 38)
+        #I conditional here to scrap bad runs
+        if vals[seed % 38] > x/3:
+            #print("bad run")
+            vals = []
+            for i in range(38):
+                vals.append(0)
+            return middle_squares(int(sys.argv[1])+1, 100)
+            
+    return queue
+
+queue = middle_squares(sys.argv[1], 100)
+
+if test == True:
+    print(vals)
+else:
+    print(queue)
--- test.sh
+++ test.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
 
-for ((i=0; i<1; i++))
+for ((i=0; i<1000; i++))
 	do
 	#get seed value
 	seed=$(./surf)
 
 	#get random numbers
-	numbers=$(python randomizer.py $seed)
+	numbers=$(python randomizer_test.py $seed)
 	
 	#print out responses
+	python histogram.py $numbers
+	echo "trial number: $i"
 	echo $numbers
 done
 
+echo "summary of values can be found in test.txt"
